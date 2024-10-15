f18d93813ba98bc43f9819a9465c9644940161cf
Vinicius <viniciussantanna2005@gmail.com>
2024-10-14 22:35:48 -0300

    page sem import desnecessario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-import { Menu } from "@/components/menu/Menu";
-import styles from "./page.module.css";
 import { Layout } from "@/components/Layout/Layout";
 
 export default function Home() {
